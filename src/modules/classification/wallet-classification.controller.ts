3c43c0d7bf2514444b0a78c2cd149556939bb606
Bryan <bryantoromo@gmail.com>
2023-07-03 11:40:33 +0300

    wallet-classification: add address & network validation + refactor classify for high availability +

--- src/modules/classification/wallet-classification.controller.ts
+++ src/modules/classification/wallet-classification.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from "../../core/decorators";
 import WalletClassificationService from "./wallet-classification.service";
-import { Param } from "@nestjs/common";
+import { BadRequestException, Param } from "@nestjs/common";
 import { ClassificationResDTO } from "./dto/response";
+import { NetworkTypeEnum, selectNetwork } from "../../core/shared/utils";
 
 
 @Controller({
@@ -17,7 +18,11 @@ export default class WalletClassificationController {
     description: "classify wallet",
     model: ClassificationResDTO,
   })
-  getTokenConfigurations(@Param("address") address: string, @Param("network") network: string): Promise<any> {
-    return this._walletClassificationService.classify({ network, walletAddress: address })
+  getWalletClassification(@Param("address") address: string, @Param("network") network: string): Promise<any> {
+    if (!address || !network) {
+      throw new BadRequestException('You need to include both network and address in the params')
+    }
+    const networkType = this._walletClassificationService.validateBeforeClassification(network, address);
+    return this._walletClassificationService.classify(networkType, address)
   }
 }
--- src/modules/classification/wallet-classification.service.ts
+++ src/modules/classification/wallet-classification.service.ts
@@ -1,13 +1,12 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { BadRequestException, Injectable, Logger } from "@nestjs/common";
 import * as ethers from 'ethers'
 import TokenConfigurationService from "../token-configuration/token-configuration.service";
 import { GetClassificationDto } from "./dto/request";
-import { ClassificationDTO } from "./dto/response";
+import { ClassificationDTO, ClassificationResDTO } from "./dto/response";
 import { WalletClassification } from "../token-configuration/token-configuration.entity";
-import CONFIG from "../../config";
 import * as util from 'util'
 import { InternalServerErrorException } from "../../core/exceptions";
-import { getWeb3Provider, validateAndRetriveABI } from "../../core/shared/utils";
+import { NetworkTypeEnum, getWeb3Provider, isValidTokenAddress, selectNetwork, validateAndRetriveABI } from "../../core/shared/utils";
 @Injectable()
 export default class WalletClassificationService {
     logger: any
@@ -17,41 +16,71 @@ export default class WalletClassificationService {
         this.logger = new Logger('WalletClassificationService');
     }
 
-    async classify(dto: GetClassificationDto) {
+    async classify(networkType: NetworkTypeEnum, walletAddress: string) {
         try {
+            const tokenConfigurations = await this._tokenConfigurationService.getAllByNetworkType(networkType);
+            this.logger.log('tokenConfigurations size', tokenConfigurations.length);
 
-            const tokenConfigurations = await this._tokenConfigurationService.getAll();
-            const classifications: ClassificationDTO[] = await Promise.all(
-                tokenConfigurations.map(async (tokenConfiguration) => {
-                    const tokenContract = new ethers.Contract(tokenConfiguration.tokenAddress, [validateAndRetriveABI(tokenConfiguration.abi)], getWeb3Provider(tokenConfiguration.networkType));
-                    const balance = await tokenContract.balanceOf(dto.walletAddress);
+            const classificationPromises = tokenConfigurations.map(async (tokenConfiguration) => {
+                try {
+                    const tokenContract = new ethers.Contract(
+                        tokenConfiguration.tokenAddress,
+                        [validateAndRetriveABI(tokenConfiguration.abi)],
+                        getWeb3Provider(tokenConfiguration.networkType)
+                    );
+                    const balance = await tokenContract.balanceOf(walletAddress);
                     const balanceNumber = parseFloat(ethers.formatUnits(balance, 0));
 
                     // Classify the wallet based on the threshold
-                    const classification = balanceNumber >= tokenConfiguration.threshold ? tokenConfiguration.classification : WalletClassification.MORTAL;
+                    const classification = balanceNumber >= tokenConfiguration.threshold
+                        ? tokenConfiguration.classification
+                        : WalletClassification.MORTAL;
+
                     return {
-                        network: dto.network,
+                        network: networkType,
                         threshold: tokenConfiguration.threshold,
                         balance: balanceNumber,
                         status: classification,
+                        tokenAddress: tokenConfiguration.tokenAddress
+                    } as ClassificationDTO;
+                } catch (error) {
+                    // Handle individual errors and return a fallback classification
+                    this.logger.error(`Error processing token configuration: ${tokenConfiguration.tokenAddress}`);
+                    this.logger.error(util.inspect(error));
+
+                    return {
+                        network: networkType,
+                        threshold: tokenConfiguration.threshold,
+                        balance: 0,
+                        status: WalletClassification.ERROR,
+                        tokenAddress: tokenConfiguration.tokenAddress
                     };
-                }),
-            );
+                }
+            });
+
+            const classifications = await Promise.allSettled(classificationPromises);
 
             return {
-                wallet: dto.walletAddress,
-                classifications,
-            }
-        } catch (e) {
-            this.logger.error(`error running [classify]: `, util.inspect(e));
-            throw new InternalServerErrorException('Something failed at [WalletClassificationService][classify]')
+                wallet: walletAddress,
+                classifications: classifications,
+            };
+        } catch (error) {
+            // Handle any errors within the classify function
+            this.logger.error(`Error running [classify]: ${util.inspect(error)}`);
+            throw new InternalServerErrorException('Something went wrong');
         }
-
     }
 
-  isNetworkSupported(networkInput:string){
 
-  }
+    validateBeforeClassification(network: string, address: string): NetworkTypeEnum {
+        const selectedNetwork = selectNetwork(network);
+        if (!selectedNetwork) {
+            throw new BadRequestException(`${network} network type is not supported, it needs to be one of : ${Object.values(NetworkTypeEnum)}`);
+        }
+        if (!isValidTokenAddress(address)) {
+            throw new BadRequestException('Invalid token address')
+        }
 
-
+        return selectedNetwork;
+    }
 }
