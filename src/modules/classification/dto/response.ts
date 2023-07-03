76027e4c5e0bb70451cacc361de8c134fd5ce862
Bryan <bryantoromo@gmail.com>
2023-07-03 12:39:34 +0300

    wallet-classification: update res-dto with swagger tags

--- src/modules/classification/dto/response.ts
+++ src/modules/classification/dto/response.ts
@@ -1,15 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { WalletClassification } from "../../../modules/token-configuration/token-configuration.entity";
 
 export class ClassificationDTO {
+  @ApiProperty()
   network: string;
+  @ApiProperty()
   threshold: number;
+  @ApiProperty()
   balance: any;
+  @ApiProperty({ enum: Object.values(WalletClassification) })
   status: WalletClassification;
-  tokenAddress:string
+  @ApiProperty()
+  tokenAddress: string
 }
 
 export class ClassificationResDTO {
+  @ApiProperty()
   wallet: string;
+  @ApiProperty()
   network: string;
+  @ApiProperty()
   classifications: ClassificationDTO[]
 }
